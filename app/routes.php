1d67c8e92ba62bfde31fc9ba02ea0cd587890930
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-15 22:39:36 +0100

    Create a route for the new index page

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,11 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+// Route::get('/', function()
+// {
+// 	return View::make('hello');
+// });
+
+
+
+Route::get("/", ["as" => "home.index", "uses" => "HomeController@index"]);
