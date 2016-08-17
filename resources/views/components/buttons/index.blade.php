5869db5e317a49da98e7a34d5bbbbe36b7d5961a
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-17 22:04:00 +0100

    Add in the content for the buttons

---
+++ resources/views/components/buttons/index.blade.php
@@ -0,0 +1,7 @@
+<div class="buttons">
+	<button class="button button--primary">Primary Button</button>
+
+	<button class="button button--secondary">Secondary Button</button>
+
+	<button class="button button--tertiary">Tertiary Button</button>
+</div>
