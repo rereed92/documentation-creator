72102aed89d4272fdf5c4e38064e9cc1b2fa0ea6
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-17 22:04:23 +0100

    Update the colours for the blocks

--- resources/views/components/blocks/index.blade.php
+++ resources/views/components/blocks/index.blade.php
@@ -12,14 +12,14 @@
 	</div>
 
 	<div class="block__item block__item--sm-1-1 block__item--md-1-2 block__item--1-3">
-		<div class="block__content block__content--blue"></div>
+		<div class="block__content block__content--orange"></div>
 	</div>
 
 	<div class="block__item block__item--sm-1-1 block__item--md-1-2 block__item--1-3">
-		<div class="block__content block__content--purple"></div>
+		<div class="block__content block__content--green"></div>
 	</div>
 
 	<div class="block__item block__item--sm-1-1 block__item--md-1-2 block__item--1-3">
-		<div class="block__content block__content--pink"></div>
+		<div class="block__content block__content--grey"></div>
 	</div>
 </div>
