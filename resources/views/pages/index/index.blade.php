69d6c4af05f8cd3650816e2959b879093d9015f4
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-17 22:04:44 +0100

    Add in the content for the nudge and buttons

--- resources/views/pages/index/index.blade.php
+++ resources/views/pages/index/index.blade.php
@@ -10,4 +10,14 @@
 	@include("components.blocks.index")
 </div>
 
+<div class="spring">
+	<h2>Nudge</h2>
+	@include("components.nudge.index")
+</div>
+
+<div class="spring">
+	<h2>Buttons</h2>
+	@include("components.buttons.index")
+</div>
+
 @endsection
