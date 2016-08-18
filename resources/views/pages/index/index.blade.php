e7bf902128247993e4cb1778247b33cfc9a01d76
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-18 21:48:06 +0100

    Add in the new sections

--- resources/views/pages/index/index.blade.php
+++ resources/views/pages/index/index.blade.php
@@ -5,19 +5,32 @@
     <h1>Documentation Creator</h1>
 </div>
 
-<div class="spring">
-	<h2>Blocks</h2>
-	@include("components.blocks.index")
+<div class="section">
+	<div class="spring">
+		<h2>Blocks</h2>
+		@include("components.blocks.index")
+	</div>
 </div>
 
-<div class="spring">
-	<h2>Nudge</h2>
-	@include("components.nudge.index")
+<div class="section">
+	<div class="spring">
+		<h2>Nudge</h2>
+		@include("components.nudge.index")
+	</div>
 </div>
 
-<div class="spring">
-	<h2>Buttons</h2>
-	@include("components.buttons.index")
+<div class="section">
+	<div class="spring">
+		<h2>Buttons</h2>
+		@include("components.buttons.index")
+	</div>
+</div>
+
+<div class="section">
+	<div class="spring">
+		<h2>Process</h2>
+		@include("components.process.index")
+	</div>
 </div>
 
 @endsection
