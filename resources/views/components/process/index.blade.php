df8ce1fb323f277a219c1ffdce3c1e4ee564c5be
Rebecca Reed <rebeccareed212@hotmail.com>
2016-08-18 21:53:05 +0100

    Create the html for the process module

---
+++ resources/views/components/process/index.blade.php
@@ -0,0 +1,59 @@
+<div class="process" data-js="_process">
+	<ul class="process__information">
+		<li class="js-process-block process__block">
+			<a href="#" class="js-process-number process__number process__number--1">
+				<p class="process__heading">Process 1</p>
+			</a>
+			<div class="js-process-text process__text">
+				<h4>Process 1</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</div>
+		</li>
+
+		<li class="js-process-block process__block">
+			<a href="#" class="js-process-number process__number process__number--2">
+				<p class="process__heading">Process 2</p>
+			</a>
+			<div class="js-process-text process__text">
+				<h4>Process 2</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</div>
+		</li>
+
+		<li class="js-process-block process__block">
+			<a href="#" class="js-process-number process__number process__number--3">
+				<p class="process__heading">Process 3</p>
+			</a>
+			<div class="js-process-text process__text">
+				<h4>Process 3</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</div>
+		</li>
+
+		<li class="js-process-block process__block">
+			<a href="#" class="js-process-number process__number process__number--4">
+				<p class="process__heading">Process 4</p>
+			</a>
+			<div class="js-process-text process__text">
+				<h4>Process 4</h4>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.
+			</div>
+		</li>
+	</ul>
+
+	<div class="process__background">
+		<img src="/images/circle.png" alt="circle" />
+	</div>
+</div>
